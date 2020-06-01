104c609332eb488ed8a9066536eb3c5c594d6d58
Zhuo Zhang <imzhuo@foxmail.com>
2020-06-01 15:14:35 +0800

    chore: adjust UI Layout
    - Making main window size equal 70% of user's current(starting) screen
    size.
    - Make image region widget & image name list widget with padding between
    window border.
    - Less Chinese comment.

--- imageset-viewer.py
+++ imageset-viewer.py
@@ -1,24 +1,32 @@
 #!/usr/bin/env python
-# coding:utf-8
-# Author: Zhuo Zhang (imzhuo@foxmail.com)
-# Date: 2017.10.22 22:36
-# Description:
-#   　浏览pascal voc 2007格式的object detection数据集图片
-#     功能：选择图片文件夹；自动寻找对应的annotation文件；图片显示bbgt；滚动显示
-# 使用的技术：
-#     Python, Tkinter(GUI), opencv(image processing), lxml(annotation parsing)
-#
-# Package requirements:
-#     cv2.so/cv2.dll
-#     pip install --upgrade image pillow lxml numpy
+# coding: utf-8
 
 from __future__ import print_function
+
+__author__ = 'Zhuo Zhang'
+__copyright__ = 'Copyright 2017-2020, Zhuo Zhang'
+__license__ = 'MIT'
+__version__ = '0.1'
+__email__ = 'imzhuo@foxmail.com'
+__status__ = 'Development'
+__description__ = 'Tkinter based GUI, visualizing PASCAL VOC object detection annotation'
+
+"""
+Changelog:
+- 2020-06-01 14:44:01
+    Draw object class name. Add license. Polish meta info.
+
+- 2017.10.22 22:36
+    Created project. Dependencies: Python, Tkinter(GUI), opencv(image processing), 
+    lxml(annotation parsing).
+    You may need this: pip install --upgrade image pillow lxml numpy
+"""
 
 try:
     import Tkinter as tk
 except:
     import tkinter as tk
-from PIL import Image, ImageTk, ImageFont, ImageDraw #pillow模块
+from PIL import Image, ImageTk, ImageFont, ImageDraw # pillow module
 import os
 try:
     from tkFileDialog import askdirectory
@@ -34,13 +42,13 @@ def zz_draw_text(im, text, text_org, font):
     Draw text on OpenCV's Image (ndarray)
     Implemented by: ndarray -> pil's image -> draw text -> ndarray
 
-    注意：OpenCV的puttext缺点：字体过大，有锯齿，不能显示中文。
+    Note: OpenCV puttext's drawback: font too large, no anti-alias, can't show Chinese chars
 
     @param im: opencv loaded image
     @param text: text(string) to be put. support Chinese
     @param font: font, e.g. ImageFont.truetype('C:/Windows/Fonts/msyh.ttc', font_size)
 
-    用法示例：
+    Example Usage:
     font_size = 20
     font = ImageFont.truetype('C:/Windows/Fonts/msyh.ttc', font_size)
     text_org = (256, 256)
@@ -54,17 +62,17 @@ def zz_draw_text(im, text, text_org, font):
 
 
 class BndBox(object):
-    def __init__(self, x1=0, y1=0, x2=0, y2=0, cls=None):
+    def __init__(self, x1=0, y1=0, x2=0, y2=0, cls_name=None):
         self.x1 = x1
         self.y1 = y1
         self.x2 = x2
         self.y2 = y2
-        self.cls_name = cls # class name
+        self.cls_name = cls_name # class name
 
 
 class PascalVOC2007XML:
     def __init__(self, xml_full_name):
-        # todo:校验xml_full_name这个文件的合法性
+        # TODO:校验xml_full_name这个文件的合法性
         self.tree = etree.parse(xml_full_name)
         self.boxes = []
 
@@ -82,8 +90,8 @@ class PascalVOC2007XML:
         return self.boxes
 
 
-class App:
-    def __init__(self, master, im_dir=None, show_x=None, show_y=None, box_thick=1):
+class VOC_Viewer(tk.Tk):
+    def __init__(self, im_dir=None, show_x=None, show_y=None, box_thick=1):
         # 加载图像：tk不支持直接使用jpg图片。需要Pillow模块进行中转
         """
         @param im_dir: 包含图片的路径，也就是"JPEGImages". 要求它的同级目录中包含Annotations目录，里面包含各种xml文件。
@@ -91,39 +99,47 @@ class App:
         @param show_y: 图片显示时的高度
         @param box_thick: 画框的宽度
         """
-        self.master = master
+        super().__init__()
+
+        # set title and window size
+        self.title('imageset viewer')
+        self.width = (int)(0.7 * self.winfo_screenwidth())
+        self.height = (int)(0.7 * self.winfo_screenheight())
+        self.geometry('%dx%d' % (self.width, self.height))
+
+        # custom settings
         self.show_x = show_x
         self.show_y = show_y
         self.box_thick = box_thick
 
         self.im_dir = tk.StringVar()
-        self.path_entry = tk.Entry(master, text=self.im_dir, width=60, state='readonly')
+        self.path_entry = tk.Entry(self, text=self.im_dir, width=60, state='readonly')
         self.path_entry.grid(row=0, column=0)
 
-        self.choose_path_btn = tk.Button(master, text='输入路径', command=self.selectPath)
+        self.choose_path_btn = tk.Button(self, text='图片路径', command=self.selectPath)
         self.choose_path_btn.grid(row=0, column=1)
 
-        ## 设定封面
+        # Surface image
         self.tkim = self.get_surface_tkim()
-        ## 也可以自己搞张图，替代默认的很丑的封面
-        #im_name = '/home/chris/Pictures/im/girl2.jpg'
-        #self.tkim = ImageTk.PhotoImage(Image.open(im_name))
+        # You may also change the surface with your favorite picture
+        # im_name = '/home/chris/Pictures/im/girl2.jpg'
+        # self.tkim = ImageTk.PhotoImage(Image.open(im_name))
 
-        self.label1 = tk.Label(master, justify='left',
+        self.labelPic = tk.Label(self, justify='left',
                           image=self.tkim, compound='center',
                           fg='white', bg='white',
-                          width = 1000, height = 1000)
-        self.label1.grid(row=1, column=0)
+                          width = self.width-250, height = self.height-50)
+        self.labelPic.grid(row=1, column=0)
 
-        self.scrollbar = tk.Scrollbar(master, orient=tk.VERTICAL)
+        self.scrollbar = tk.Scrollbar(self, orient=tk.VERTICAL)
 
-        self.listbox = tk.Listbox(master, yscrollcommand=self.scrollbar.set)
+        self.listbox = tk.Listbox(self, yscrollcommand=self.scrollbar.set)
         self.listbox.grid(row=1, column=2, sticky=tk.N+tk.S)
 
         self.im_names = []
         if im_dir is not None:
             self.im_dir.set(im_dir)
-            #获取自然顺序的文件列表
+            # Get natural file list, same as Windows explorer's result
             self.im_names = [_ for _ in os.listdir(self.im_dir.get())]
             self.im_names.sort()
             for im_name in self.im_names:
@@ -139,8 +155,8 @@ class App:
             if (im_name.endswith('.jpg') or im_name.endswith('.png')):
                 im_name_full = os.path.join(self.im_dir.get(), im_name).replace('\\', '/')
                 self.tkim = self.get_tkim(im_name_full)
-                self.label1.configure(image=self.tkim)
-                #print(im_name_full)
+                self.labelPic.configure(image=self.tkim)
+                # print(im_name_full)
 
     def get_tkim(self, im_name_full):
         """
@@ -177,16 +193,12 @@ class App:
                           color=(0, 255, 0),
                           thickness=self.box_thick
                           )
-                # cv2.putText(im, "object",
-                #     (xmin+10, ymin-20),
-                #     cv2.FONT_HERSHEY_COMPLEX, 1, (0, 255, 0), 5
-                # )
                 font_size = 16
                 font = ImageFont.truetype('‪C:/Windows/Fonts/msyh.ttc', font_size)
                 text_org = (xmin+10, ymin+10)
                 im = zz_draw_text(im, box.cls_name, text_org, font)
 
-        im = im[:, :, ::-1]  #bgr => rgb   necessary
+        im = im[:, :, ::-1]  # bgr => rgb   necessary
         tkim = ImageTk.PhotoImage(Image.fromarray(im))
         return tkim
 
@@ -206,7 +218,6 @@ class App:
         return tkim
 
     def get_boxes_from_voc_xml(self, anno_name_full):
-        # 解析xml文件，获取bbox
         anno = PascalVOC2007XML(anno_name_full)
         boxes = anno.get_boxes()
         return boxes
@@ -214,7 +225,7 @@ class App:
     def selectPath(self):
         pth = askdirectory()
 
-        #清空listbox中的元素
+        # 清空listbox中的元素
         self.listbox.delete(0, len(self.im_names)-1)
 
         self.fill_im_names(pth)
@@ -228,14 +239,9 @@ class App:
             for im_name in self.im_names:
                 self.listbox.insert(tk.END, im_name)
 
-# example usage:
 if __name__ == '__main__':
-    root = tk.Tk()  #创建窗口对象的背景色
-    root.title('imageset viewer')
-    root.geometry('1400x1400') #设置窗口大小
-
     ## 最简单的方式：不预设im_dir，打开GUI后自行选择图片路径
-    app = App(root, im_dir=None, box_thick=2)
+    app = VOC_Viewer(im_dir=None, box_thick=2)
 
     """
     ## 也可以在代码中指定
@@ -252,5 +258,5 @@ if __name__ == '__main__':
     app = App(root, im_dir, show_x=1000, show_y=1000, box_thick=2)
     """
 
-    #进入消息循环
-    root.mainloop()
+    # message loop
+    app.mainloop()
