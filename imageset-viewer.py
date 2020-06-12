387875ff7bf90e4cdcf66e1f93c7a29ca58161ed
Zhuo Zhang <imzhuo@foxmail.com>
2020-06-12 22:49:28 +0800

    fix: add 'JPG' for supported type

--- imageset-viewer.py
+++ imageset-viewer.py
@@ -160,7 +160,8 @@ class VOC_Viewer(tk.Tk):
         self.num_classes = len(self.cls_names)
         self.color_table = get_color_table(self.num_classes)
         self.class_to_ind = dict(zip(self.cls_names, range(self.num_classes)))
-        self.supported_im_ext = ['bmp', 'png', 'jpg', 'jpeg', 'JPEG', 'PNG']
+        self.supported_im_ext = ['bmp', 'png', 'jpg', 'jpeg', 
+                                 'BMP', 'PNG', 'JPG', 'JPEG']
 
     def get_color_by_cls_name(self, cls_name):
         ind = self.class_to_ind[cls_name]
